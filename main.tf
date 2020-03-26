ad98515a18020b47d7946e5364b7182f606153f1
John Leach <john@johnleach.co.uk>
2020-03-26 21:42:01 +0000

    Initial import

---
+++ main.tf
@@ -0,0 +1,126 @@
+variable "account" {
+  type        = string
+  description = "Brightbox account identifier for api authentication by terraform"
+}
+variable "username" {
+  type        = string
+  description = "brightbox username for api authentication by terraform"
+}
+variable "password" {
+  type        = string
+  description = "brightbox password for api authentication by terraform"
+}
+
+variable "cluster_name" {
+  type        = string
+  description = "Name to identify the cluster. Used in resource names"
+  default     = "test"
+}
+variable "server_type" {
+  type        = string
+  description = "type of server to create to run jitsi"
+  default     = "4gb.ssd"
+}
+variable "acme_email" {
+  type        = string
+  description = "email address to register with Let's Encrypt"
+}
+variable "jitsi_fqdn" {
+  type        = string
+  description = "allow overriding fqdn used to access jitsi. lets encrypt certificate will be generated for this. Defaults to Cloud IP fqdn"
+  default     = ""
+}
+
+locals {
+  jitsi_fqdn = coalesce(var.jitsi_fqdn, brightbox_cloudip.jitsi.fqdn)
+}
+
+provider "brightbox" {
+  version   = "~> 1.2"
+  apiurl    = "https://api.gb1.brightbox.com"
+  account   = var.account
+  username  = var.username
+  password  = var.password
+}
+
+data "brightbox_image" "bionic" {
+  name        = "^ubuntu-bionic.*server$"
+  arch        = "x86_64"
+  official    = true
+  most_recent = true
+}
+
+resource "brightbox_server_group" "jitsi" {
+  name = "jitsi.${var.cluster_name}"
+}
+
+resource "brightbox_firewall_policy" "jitsi" {
+  name         = brightbox_server_group.jitsi.name
+  server_group = brightbox_server_group.jitsi.id
+}
+
+resource "brightbox_firewall_rule" "jitsi-tcp" {
+  destination_port = "80,443,4443,22"
+  protocol         = "tcp"
+  source           = "any"
+  description      = "jitsi services"
+  firewall_policy  = brightbox_firewall_policy.jitsi.id
+}
+resource "brightbox_firewall_rule" "jitsi-udp" {
+  destination_port = "10000"
+  protocol         = "udp"
+  source           = "any"
+  description      = "jitsi services"
+  firewall_policy  = brightbox_firewall_policy.jitsi.id
+}
+resource "brightbox_firewall_rule" "jitsi-out" {
+  destination      = "any"
+  description      = "jitsi outgoing"
+  firewall_policy  = brightbox_firewall_policy.jitsi.id
+}
+
+resource "brightbox_cloudip" "jitsi" {
+  name   = "jitsi.${var.cluster_name}"
+  target = brightbox_server.jitsi.interface
+
+  provisioner "local-exec" {
+    when    = destroy
+    command = "ssh-keygen -R ${self.fqdn}; ssh-keygen -R ${self.public_ip}"
+  }
+}
+
+resource "brightbox_server" "jitsi" {
+  name      = "jitsi.${var.cluster_name}"
+  image     = data.brightbox_image.bionic.id
+  type      = var.server_type
+  server_groups = [brightbox_server_group.jitsi.id]
+}
+
+resource "null_resource" "configure-jitsi" {
+
+  triggers  = {
+    server = brightbox_server.jitsi.id
+    cloudip = brightbox_cloudip.jitsi.public_ip
+    jitsi_fqdn = local.jitsi_fqdn
+  }
+
+  connection {
+    user = "ubuntu"
+    host = brightbox_cloudip.jitsi.fqdn
+  }
+
+  provisioner "file" {
+    content     = file("scripts/configure-jitsi.sh")
+    destination = "configure-jitsi.sh"
+  }
+
+  provisioner "remote-exec" {
+    inline = [
+      "sudo sh /home/ubuntu/configure-jitsi.sh ${local.jitsi_fqdn} ${brightbox_cloudip.jitsi.fqdn} ${var.acme_email}"
+    ]
+  }
+}
+
+output "jitsi_url" {
+  value = "https://${local.jitsi_fqdn}"
+}
---
+++ scripts/configure-jitsi.sh
@@ -0,0 +1,45 @@
+#!/bin/bash
+
+FQDN=$1
+CIP=$2
+ACME_EMAIL=$3
+
+
+echo "Checking DNS is setup right..."
+FQDN_IPS=$(host $FQDN | grep -oE "address (.*)" | awk '{print $2}')
+
+if [ -z "$FQDN_IPS" ] ; then
+    echo "FQDN $FQDN doesn't resolve"
+    exit 1
+fi
+
+
+for fqdn_ip in $FQDN_IPS ; do
+    echo "Checking $fqdn_ip points at me..."
+    if ! (host $CIP | grep -q $fqdn_ip) ; then
+        echo "FQDN $FQDN has IP address $fqdn_ip which doesn't resolve to the cloud ip $CIP"
+    fi
+done
+
+apt-add-repository 'deb https://download.jitsi.org stable/' >/dev/null
+wget -qO - https://download.jitsi.org/jitsi-key.gpg.key | sudo apt-key add -
+apt-get install -qy debconf-utils
+
+debconf-set-selections <<EOF
+jicofo	jitsi-videobridge/jvb-hostname	string	$FQDN
+jitsi-meet-prosody	jitsi-videobridge/jvb-hostname	string	$FQDN
+jitsi-meet-web-config	jitsi-videobridge/jvb-hostname	string	$FQDN
+jitsi-videobridge	jitsi-videobridge/jvb-hostname	string	$FQDN
+jitsi-meet-prosody	jitsi-meet-prosody/jvb-hostname	string	$FQDN
+jitsi-meet-web-config	jitsi-meet/cert-choice	select	Generate a new self-signed certificate (You will later get a chance to obtain a Let's encrypt certificate)
+EOF
+
+export DEBIAN_FRONTEND=noninteractive
+
+if ! dpkg-query -W jitsi-meet ; then
+    apt-get install -qy jitsi-meet
+else
+    dpkg-reconfigure jitsi-videobridge jitsi-meet jitsi-meet-prosody jicofo jitsi-meet-web-config
+fi
+
+echo $ACME_EMAIL | /usr/share/jitsi-meet/scripts/install-letsencrypt-cert.sh
